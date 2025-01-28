2505ae2705cccac91d4d949cabb93f3d6dc3ad4b
Sebhvarg <119546442+Sebhvarg@users.noreply.github.com>
2025-01-28 16:50:43 -0500

    Conexión de Arduino establecida

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import tkinter as tk 
-from ui.app import App
-import serial # type: ignore
+import serial 
 import time
+from logic.graph import Graph
+from logic.pathfinding import shortest_path
+
+
 # Comunicarse con el arduino
+
+import serial
+import time
 
 def arduino_connection():
     try:
-        arduino = serial.Serial('COM3', baudrate= 9600, timeout=1.0)
-        time.sleep(2)
+        arduino = serial.Serial('COM3', baudrate=9600, timeout=2.0)
+        time.sleep(2)  # Asegúrate de darle tiempo a Arduino para establecer la conexión
         print("Conectado a Arduino")
         return arduino
-    except: serial.SerialException
-    print("Error al conectar con Arduino")
-    return None
+    except serial.SerialException as e:
+        print(f"Error al conectar con Arduino: {e}")
+        return None
+
 
 
 # Enviar ruta optima al arduino
@@ -32,5 +44,34 @@ def send_route_to_arduino(arduino, start, end):
 #Main
 
 def main():
-
+    
     arduino = arduino_connection()
+    
+    # Crear Grafo 
+
+    graph = Graph()
+    graph.add_node("FIEC")
+    graph.add_node("FCV")
+    graph.add_node("FIMCP")
+    graph.add_node("FADCOM")
+    graph.add_node("FCNM")
+
+    graph.add_edge("FIEC", "FIMCP", 1)
+    graph.add_edge("FIEC", "FCNM", 1)
+    graph.add_edge("FIMCP", "FCV", 1)
+    graph.add_edge("FCV", "FCNM", 1)
+    graph.add_edge("FIMCP", "FCNM", 1)
+
+    ventana = tk.Tk()
+    ventana.title("GAYCar")
+    ventana.geometry("1200x1600")
+
+    ventana.configure(bg = "black")
+
+
+if __name__ == "__main__":
+    main()
+        
+    print("Fin de la ejecución")
+    
+
