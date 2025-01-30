b500f046cc73d4e48004a392a1665ca18421c613
Sebhvarg <119546442+Sebhvarg@users.noreply.github.com>
2025-01-30 16:13:34 -0500

    Agregar archivo

--- server/servidor.py
+++ server/servidor.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Ruta donde se guardarán las coordenadas
+FILE_PATH = "coordenadas.txt"
 
 @app.route('/coordenadas', methods=['GET'])
 def recibir_coordenadas():
@@ -9,11 +11,15 @@ def recibir_coordenadas():
     lon = request.args.get('lon')
 
     if lat and lon:
-        print(f"📍 Coordenadas recibidas: Latitud = {lat}, Longitud = {lon} \n ")
+        print(f"📍 Coordenadas recibidas: Latitud = {lat}, Longitud = {lon} \n")
+
+        # Guardar coordenadas en un archivo de texto
+        with open(FILE_PATH, "a") as file:
+            file.write(f"{lat},{lon}\n")
+
         return {"status": "OK", "latitud": lat, "longitud": lon}, 200
     else:
         return {"error": "Faltan parámetros lat y lon"}, 400
-
 
 if __name__ == '__main__':
     app.run(host='192.168.3.12', port=5000)
