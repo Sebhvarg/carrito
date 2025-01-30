85a1f822e6d8fd1141355c16b80d6a5b25ee27c8
Sebhvarg <119546442+Sebhvarg@users.noreply.github.com>
2025-01-30 17:11:24 -0500

    correcion ui

--- test/tempCodeRunnerFile.py
+++ test/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-shortest_path
+# archivo: main.py
+
+import sys
+import os
+import tkinter as tk
+
+# Asegúrate de que el directorio 'ui' está en el path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "ui")))
--- ui/app.py
+++ ui/app.py
@@ -1,4 +1,12 @@
 # archivo: ui/app.py
+# archivo: main.py
+
+import sys
+import os
+import tkinter as tk
+
+# Asegúrate de que el directorio 'ui' está en el path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 import networkx as nx
 from geopy.distance import geodesic
